c2703ad5a14a64369eba6c0f226723efb042b476
Devin Cenatiempo <dcenatiempo@gmail.com>
2018-01-23 08:02:34 -0700

    Just getting things set up: Topic 6

--- topic_6/main.js
+++ topic_6/main.js
@@ -6,11 +6,41 @@
  *   removeChild
  *   etc.
 *******************************************************************************/
+var counter = 1;
+var body = document.getElementsByTagName('body');
+console.log(body[0])
 
+document.getElementById('add-begin').addEventListener('click', function() {
+  counter++;
+  var newDiv = createDiv(counter);
+  body.insertBefore(newDiv, body.children[1]);
+  // Insert element2 as child of element 1, right before element3
+element1.insertBefore(element2, element3)
+});
+document.querySelector('#add-begin').addEventListener('click', function() {
+  
+});
 /***************************************************
- * 
+ * createElement
  **************************************************/
-
+function createDiv(num) {
+  var newDiv = document.createElement('div');
+  newDiv.appendChild(
+    document.createElement('h2')
+      .appendChild(
+        createTextNode(`Element #${num}`)));
+  newDiv.appendChild(
+    document.createElement('button')
+      .appendChild(
+        createTextNode('Click to change color'))
+      .classList.add('change-color'));
+  newDiv.appendChild(
+    document.createElement('button')
+      .appendChild(
+        createTextNode('Click to Remove me'))
+      .classList.add('remove'));
+  return newDiv;
+};
   /*****************************
    * 
    ****************************/
